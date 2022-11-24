96c41852661989475c191721f92d0b802ae391ac
amribader <91694262+amribader@users.noreply.github.com>
2022-11-24 22:47:25 +0100

    fine prima parte mappe passivo funziona base

--- routes/screens/TwokOnMap.jsx
+++ routes/screens/TwokOnMap.jsx
@@ -93,7 +93,7 @@ const styles = StyleSheet.create({
 
 import React, { useState, useEffect } from 'react';
 import MapView, { Callout, Circle, Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, Button } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Button, Alert } from 'react-native';
 import * as Location from 'expo-location';
 const TwokOnMap = ({ navigation, route }) => {
     const [pin, setPin] = useState({
@@ -117,69 +117,103 @@ const TwokOnMap = ({ navigation, route }) => {
     }
 
     useEffect(() => {
+
+        !route.params.lat && !route.params.lon ? alert() : '';
+
         userLocation();
         console.log(route.params)
         console.log(region)
     }, []);
+
+
+    const alert = () => {
+        Alert.alert(
+            'QUESTO TWOK NON è GEOLOCALIZZATO!!',
+            'Tornare alla Bacheca??',
+            [
+                {
+                    text: 'Cancel',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: "cancel"
+                },
+                { text: "OK", onPress: () => navigation.goBack() }
+            ]
+        )
+    }
+
     return (
         <View style={styles.container}>
-            <View>
-                <Text>CIAO A TUTTI</Text>
+{
+    route.params.lat ? ( <>
+        <View style={styles.viewButton}>
+            <Text>CIAO A TUTTI</Text>
 
-                <Button
-                    title="Back to Home"
-                    color="#841584"
-                    onPress={() => navigation.navigate('Bacheca')}
-                />
-            </View>
+            <Button
+                title="Back to Home"
+                color="#841584"
+                onPress={() => {
+                    navigation.goBack()
+                    console.log('BARRACUDA')
+                    return
+                }}
+            />
+        </View>
 
-            <MapView initialRegion={region}
-                provider="google" style={styles.map}
-                zoomEnabled={true}
-                zoomTapEnabled={true}
-                zoomControlEnabled={true}
-                rotateEnabled={true}
+        <MapView initialRegion={region}
+            provider="google" style={styles.map}
+            zoomEnabled={true}
+            zoomTapEnabled={true}
+            zoomControlEnabled={true}
+            rotateEnabled={true}
+        >
+
+
+
+            <Marker
+                coordinate={pin}
+                pinColor="black"
+                draggable={true}
+                onDragStart={(e) => {
+                    console.log("Drag start", e.nativeEvent.coordinates)
+                }}
+                onDragEnd={(e) => {
+                    setPin({
+                        latitude: e.nativeEvent.coordinate.latitude,
+                        longitude: e.nativeEvent.coordinate.longitude
+                    })
+                }}
             >
+                <Callout>
+                    <Text>I'm here</Text>
+                </Callout>
+            </Marker>
+            <Circle center={pin} radius={1000} />
 
-
-
-                <Marker
-                    coordinate={pin}
-                    pinColor="black"
-                    draggable={true}
-                    onDragStart={(e) => {
-                        console.log("Drag start", e.nativeEvent.coordinates)
-                    }}
-                    onDragEnd={(e) => {
-                        setPin({
-                            latitude: e.nativeEvent.coordinate.latitude,
-                            longitude: e.nativeEvent.coordinate.longitude
-                        })
-                    }}
-                >
-                    <Callout>
-                        <Text>I'm here</Text>
-                    </Callout>
-                </Marker>
-                <Circle center={pin} radius={1000} />
-
-            </MapView>
+        </MapView>
+    </> ):(<Text>ERRORE</Text>)
+}
+           
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        //flex: 1,
+        flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    viewButton: {
+        //flex:2,
+        marginTop: 40,
+    },
     map: {
+        //flex: 2,
         width: Dimensions.get('window').width,
-        //height: (Dimensions.get('window').height) - 600,
-        height: '100%',
-        marginTop: 180,
+        //height: (Dimensions.get('window').height),
+        height: '90%',
+        // marginTop: 180,
     },
 });
 
